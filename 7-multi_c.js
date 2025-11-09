dfe8de8fed9c6f7e12279d067f95d979b2d81ec7
amaredw2024 <your_email@example.com>
2025-11-09 20:33:00 +0300

    my

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,15 +1,15 @@
 // Get the first argument
 const arg = process.argv[2];
 
-// Convert to integer
+// Convert it to an integer
 const count = parseInt(arg, 10);
 
 // Check if the number is valid
 if (isNaN(count) || count <= 0) {
     console.log("Missing number of occurrences");
 } else {
-    // Loop to print "C is fun" count times
     let i = 0;
+    // Loop to print "C is fun" count times
     while (i < count) {
         console.log("C is fun");
         i++;
