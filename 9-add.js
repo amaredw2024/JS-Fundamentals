1846f8307ff7925ab14f587038ee2da91b3c36c6
amaredw2024 <your_email@example.com>
2025-11-09 20:17:40 +0300

    my last commit

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+// Function that adds two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Get the first and second arguments
+const arg1 = parseInt(process.argv[2], 10);
+const arg2 = parseInt(process.argv[3], 10);
+
+// Print the result of the addition
+console.log(add(arg1, arg2));
