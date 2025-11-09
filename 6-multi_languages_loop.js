3a2a5eaf7e58b776d02e239d8b5b7aa4d0a72917
amaredw2024 <your_email@example.com>
2025-11-09 19:57:33 +0300

    my seventh commit

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of messages
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each message
+for (let i = 0; i < messages.length; i++) {
+    console.log(messages[i]);
+}
